8df6b43789ca9ba232e672f5236e99d923aff0df
Developer <developer@example.com>
2026-04-08 11:23:51 +0530

    number changes

--- app/privacy-policy/page.js
+++ app/privacy-policy/page.js
@@ -287,7 +287,7 @@ export default function PrivacyPolicy() {
                   <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M6.6 10.8c1.4 2.8 3.8 5.1 6.6 6.6l2.2-2.2c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.6.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1-9.4 0-17-7.6-17-17 0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1 0 1.3.2 2.5.6 3.6.1.3 0 .7-.2 1L6.6 10.8z" />
                   </svg>
-                  Customer Care: 18001031041
+                  9718344024
                 </a>
               </div>
             </div>
